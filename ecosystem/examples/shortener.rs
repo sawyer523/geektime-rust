59e635ca887d1c7384dbc64b517669436cdf4dd2
xiaonan chen <sawyer523@163.com>
2024-05-27 15:37:54 +0800

    feature: homework4

--- ecosystem/examples/shortener.rs
+++ ecosystem/examples/shortener.rs
@@ -5,12 +5,14 @@ use axum::{
     response::IntoResponse,
     Router, routing::{get, post},
 };
+use axum::response::Response;
 use http::{header::LOCATION, HeaderMap, StatusCode};
 use nanoid::nanoid;
 use serde::{Deserialize, Serialize};
-use sqlx::{FromRow, PgPool};
+use sqlx::{Error, FromRow, PgPool};
+use thiserror::Error;
 use tokio::net::TcpListener;
-use tracing::{info, level_filters::LevelFilter, warn};
+use tracing::{info, level_filters::LevelFilter};
 use tracing_subscriber::{fmt::Layer, Layer as _, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Debug, Deserialize)]
@@ -34,6 +36,16 @@ struct UrlRecord {
     id: String,
     #[sqlx(default)]
     url: String,
+}
+
+#[derive(Debug, Error)]
+pub enum ShortenerError {
+    #[error("Unprocessable entity: {0}")]
+    Unprocessable(String),
+    #[error("Not found: {0}")]
+    Notfound(String),
+    #[error("Database error")]
+    Database(#[from] sqlx::Error)
 }
 
 const LISTEN_ADDR: &str = "127.0.0.1:9876";
@@ -62,29 +74,38 @@ async fn main() -> Result<()> {
 async fn shorten(
     State(state): State<AppState>,
     Json(data): Json<ShortenReq>,
-) -> Result<impl IntoResponse, StatusCode> {
-    let id = state.shorten(&data.url).await.map_err(|e| {
-        warn!("Failed to shorten URL: {e}");
-        StatusCode::UNPROCESSABLE_ENTITY
-    })?;
-    let body = Json(ShortenRes {
-        url: format!("http://{}/{}", LISTEN_ADDR, id),
-    });
-    Ok((StatusCode::CREATED, body))
+) -> Result<impl IntoResponse, ShortenerError> {
+    info!("Shorten request: {:?}", data);
+    let ret = state.shorten(&data.url).await;
+    match ret {
+        Ok(id) => {
+            let body = Json(ShortenRes {
+                url: format!("http://{}/{}", LISTEN_ADDR, id),
+            });
+            Ok((StatusCode::CREATED, body))
+        },
+        Err(e) => Err(e)
+    }
 }
 
 async fn redirect(
     Path(id): Path<String>,
     State(state): State<AppState>,
-) -> Result<impl IntoResponse, StatusCode> {
-    let url = state
+) -> Result<impl IntoResponse, ShortenerError> {
+    let ret = state
         .get_url(&id)
-        .await
-        .map_err(|_| StatusCode::NOT_FOUND)?;
-
-    let mut headers = HeaderMap::new();
-    headers.insert(LOCATION, url.parse().unwrap());
-    Ok((StatusCode::PERMANENT_REDIRECT, headers))
+        .await;
+    match ret { 
+        Ok(url) => {
+            let mut headers = HeaderMap::new();
+            headers.insert(LOCATION, url.parse().unwrap());
+            Ok((StatusCode::PERMANENT_REDIRECT, headers))
+        },
+        Err(e) => {
+            Err(e)
+        }
+        
+    }
 }
 
 impl AppState {
@@ -104,23 +125,49 @@ impl AppState {
         Ok(Self { db: pool })
     }
 
-    async fn shorten(&self, url: &str) -> Result<String> {
-        let id = nanoid!(6);
-        let ret: UrlRecord = sqlx::query_as(
-            "INSERT INTO urls (id, url) VALUES ($1, $2) ON CONFLICT(url) DO UPDATE SET url=EXCLUDED.url RETURNING id",
-        )
-            .bind(&id)
-            .bind(url)
-            .fetch_one(&self.db)
-            .await?;
-        Ok(ret.id)
+    async fn shorten(&self, url: &str) -> Result<String, ShortenerError> {
+        loop {
+            let id = nanoid!(6);
+            match sqlx::query_as::<_, UrlRecord>(
+                "INSERT INTO urls (id, url) VALUES ($1, $2) ON CONFLICT(url) DO UPDATE SET url=EXCLUDED.url RETURNING id",
+            )
+                .bind(&id)
+                .bind(url)
+                .fetch_one(&self.db)
+                .await {
+                Ok(ret) => return Ok(ret.id),
+                Err(e) => match e {
+                    Error::RowNotFound => continue,
+                    _ => return Err(ShortenerError::Database(e)),
+                }
+            }
+        }
     }
 
-    async fn get_url(&self, id: &str) -> anyhow::Result<String> {
-        let ret: UrlRecord = sqlx::query_as("SELECT url FROM urls WHERE id = $1")
+    async fn get_url(&self, id: &str) -> Result<String, ShortenerError> {
+        match sqlx::query_as::<_, UrlRecord>("SELECT * FROM urls WHERE id = $1")
             .bind(id)
             .fetch_one(&self.db)
-            .await?;
-        Ok(ret.url)
+            .await {
+            Ok(record) => Ok(record.url),
+            Err(e) => {
+                match e {
+                    Error::RowNotFound => Err(ShortenerError::Notfound(id.into())),
+                    _ => Err(ShortenerError::Database(e))
+                }
+            }
+        }
+    }
+}
+
+impl IntoResponse for ShortenerError {
+    fn into_response(self) -> Response {
+        match self {
+            ShortenerError::Unprocessable(msg) => {
+                (StatusCode::UNPROCESSABLE_ENTITY, msg).into_response()
+            }
+            ShortenerError::Notfound(msg) => (StatusCode::NOT_FOUND, format!("Url {} not found", msg)).into_response(),
+            ShortenerError::Database(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Database error").into_response(),
+        }
     }
 }
