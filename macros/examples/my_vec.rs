4c1fcf2ff03a2685742036466ddc0f611d149012
xiaonan chen <sawyer523@163.com>
2024-06-03 00:42:30 +0800

    macros

---
+++ macros/examples/my_vec.rs
@@ -0,0 +1,17 @@
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let v: Vec<i32> = my_vec![
+        "1".parse()?,
+        "2".parse()?,
+        "3".parse()?,
+        "4".parse()?,
+        "5".parse()?,
+        "6".parse()?,
+    ];
+
+    //let v = vec![1, 2, 3];
+    println!("{:?}", v);
+
+    Ok(())
+}
